3f834d1dc911215a53bae7ca56712cdffd967bff
ashnar2022 <ashnamearn2022@gmail.com>
2023-04-19 23:20:26 +0530

    netflixclone

---
+++ src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Nav.css'
+
+function Nav() {
+  return (
+    <div className='nav'>
+        <img className='logo' src='https://upload.wikimedia.org/wikipedia/commons/7/7a/Logonetflix.png'/>
+    </div>
+  )
+}
+
+export default Nav
